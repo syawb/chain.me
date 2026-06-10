3d3ecf6181089187b6cf7a375212c43ea6eed61e
syawb <ziz.syaw@gmail.com>
2026-06-10 12:45:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function isValidAddress(address) {
+    return /^0x[a-fA-F0-9]{40}$/.test(address);
+}
+
+document
+    .getElementById("trackBtn")
+    .addEventListener("click", () => {
+
+        const address =
+            document.getElementById("wallet").value.trim();
+
+        const result =
+            document.getElementById("result");
+
+        if (!isValidAddress(address)) {
+            alert("Invalid wallet address");
+            return;
+        }
+
+        const data = {
+            ethereum: "0.52 ETH",
+            bnb: "1.8 BNB",
+            polygon: "250 MATIC",
+            arbitrum: "0.15 ETH"
+        };
+
+        result.innerHTML = `
+            <h2>Wallet: ${address}</h2>
+
+            <div class="card">
+                <h3>Ethereum</h3>
+                <p>${data.ethereum}</p>
+            </div>
+
+            <div class="card">
+                <h3>BNB Chain</h3>
+                <p>${data.bnb}</p>
+            </div>
+
+            <div class="card">
+                <h3>Polygon</h3>
+                <p>${data.polygon}</p>
+            </div>
+
+            <div class="card">
+                <h3>Arbitrum</h3>
+                <p>${data.arbitrum}</p>
+            </div>
+        `;
+});
